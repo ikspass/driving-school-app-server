62d77e3ee14abfece1dab6aa33ca2f944d204d94
ikspass <moolingtoon@mail.ru>
2025-04-29 20:56:09 +0300

    init project

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello() {
+    return {
+      message: 'Hello World!',
+    };
+  }
+
+  @Post()
+  saveInfo(@Body()  body: any): string {
+    return {
+      ...body,
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-async function start() {
-  const PORT = process.env.PORT || 5000;
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-start();
+bootstrap();
