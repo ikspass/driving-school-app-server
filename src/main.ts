99e366111975a0966403072e7fa727b9133cab88
ikspass <moolingtoon@mail.ru>
2025-05-16 16:58:48 +0300

    add index.ts

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,4 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cors from 'cors';
-import { join } from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import start from './index';
 
-async function start() {
-  const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  // Настройка статических файлов
-  app.useStaticAssets(join(__dirname, '..', 'static')); // Указываем папку для статических файлов
-
-  const config = new DocumentBuilder()
-    .setTitle('Driving School App')
-    .setDescription('Diploma project')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api/docs', app, document);
-
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
-
-  await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-}
-
-export default start;
+// Запускаем приложение
+start();
